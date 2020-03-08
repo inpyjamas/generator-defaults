1c15239e5b454047f9909428f171ba196c07e1f1
fabianmoronzirfas <fabianmoronzirfas@protonmail.ch>
2020-03-08 09:03:50 +0100

    test(add first test)

--- src/__tests__/inpyjamas.test.ts
+++ src/__tests__/inpyjamas.test.ts
@@ -1,17 +1,25 @@
 import helpers from "yeoman-test";
 import path from "path";
+import rimraf from "rimraf";
+import assert from "yeoman-assert";
+import fs from "fs";
 // TODO: Read https://medium.com/@adamdziendziel/how-to-write-tests-for-yeoman-generator-9376ea94201
+// TODO: Make folders of templates and common files relative to all files
 afterEach(() => {
-  rimraf.sync(path.join(__dirname, ‘tmp’));
- });
+  rimraf.sync(path.join(__dirname, "tmp"));
+});
 describe("in pyjamas", () => {
+  // eslint-disable-next-line jest/no-test-callback
   test("should pass", async done => {
+    const testFolderPath = path.join(__dirname, "tmp");
     const res = await helpers
-      .run(path.join(__dirname, "../index"))
-      .inDir(path.join(__dirname, "tmp"))
+      .run(path.join(__dirname, "../../index"))
+      .inDir(path.join(testFolderPath))
       .withPrompts({ name: "foo", type: "typescript-express" });
     console.log(res);
-    expect(2).toBe(2);
+    expect(fs.existsSync(`${testFolderPath}/src/index.ts`)).toBe(true);
+    expect(assert.file(`${testFolderPath}/src/index.ts`)).not.toThrow();
+
     done();
   });
 });
