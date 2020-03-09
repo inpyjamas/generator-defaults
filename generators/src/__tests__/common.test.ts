a39dbd333645d6d8d49bbfee8f2d62461d8e935a
fabianmoronzirfas <fabianmoronzirfas@protonmail.ch>
2020-03-09 09:53:03 +0100

    refactor(tests): move cli call to beforeEach function

--- generators/src/__tests__/common.test.ts
+++ generators/src/__tests__/common.test.ts
@@ -4,53 +4,35 @@ import rimraf from "rimraf";
 // import assert from "yeoman-assert";
 import fs from "fs";
 import { toLowerCase } from "../lib/util";
-
+const testFolderPath = path.join(__dirname, "tmp");
+beforeEach(() => {
+  return helpers
+    .run(path.resolve(__dirname, "../index"))
+    .inDir(path.join(testFolderPath))
+    .withPrompts({
+      name: "foo",
+      type: "typescript-express",
+      upgrade: false
+    });
+});
 afterAll(() => {
   rimraf.sync(path.resolve(__dirname, "tmp"));
 });
 describe("typescript-express template test", () => {
   // eslint-disable-next-line jest/no-test-callback
-  test("should use the provided name in templates", async done => {
-    try {
-      const testFolderPath = path.join(__dirname, "tmp");
-      await helpers
-        .run(path.resolve(__dirname, "../index"))
-        .inDir(path.join(testFolderPath))
-        .withPrompts({
-          name: "foo",
-          type: "typescript-express",
-          upgrade: false
-        });
-      const pkg = JSON.parse(
-        fs.readFileSync(`${testFolderPath}/package.json`, "utf8")
-      );
-      expect(pkg.name).toBe("foo");
-      done();
-    } catch (error) {
-      done();
-    }
+  test("should use the provided name in templates", () => {
+    const pkg = JSON.parse(
+      fs.readFileSync(`${testFolderPath}/package.json`, "utf8")
+    );
+    expect(pkg.name).toBe("foo");
   });
 
   // eslint-disable-next-line jest/no-test-callback
-  test("should make the provided name lowercase", async done => {
-    try {
-      const testFolderPath = path.join(__dirname, "tmp");
-      await helpers
-        .run(path.resolve(__dirname, "../index"))
-        .inDir(path.join(testFolderPath))
-        .withPrompts({
-          name: "Foo",
-          type: "typescript-express",
-          upgrade: false
-        });
-      const pkg = JSON.parse(
-        fs.readFileSync(`${testFolderPath}/package.json`, "utf8")
-      );
-      expect(pkg.name).toBe("foo");
-      done();
-    } catch (error) {
-      done();
-    }
+  test("should make the provided name lowercase", () => {
+    const pkg = JSON.parse(
+      fs.readFileSync(`${testFolderPath}/package.json`, "utf8")
+    );
+    expect(pkg.name).toBe("foo");
   });
 
   test("to lower case", () => {
--- generators/src/__tests__/typescript-express.test.ts
+++ generators/src/__tests__/typescript-express.test.ts
@@ -4,34 +4,31 @@ import rimraf from "rimraf";
 // import assert from "yeoman-assert";
 import fs from "fs";
 // TODO: Read https://medium.com/@adamdziendziel/how-to-write-tests-for-yeoman-generator-9376ea94201
+const testFolderPath = path.join(__dirname, "tmp");
 afterAll(() => {
   rimraf.sync(path.resolve(__dirname, "tmp"));
 });
+
+beforeEach(() => {
+  return helpers
+    .run(path.resolve(__dirname, "../index"))
+    .inDir(path.join(testFolderPath))
+    .withPrompts({
+      name: "foo",
+      type: "typescript-express",
+      upgrade: false
+    });
+});
 describe("typescript-express template test", () => {
   // eslint-disable-next-line jest/no-test-callback
-  test("should create all the files in tmp directory", async done => {
-    try {
-      const testFolderPath = path.join(__dirname, "tmp");
-      await helpers
-        .run(path.resolve(__dirname, "../index"))
-        .inDir(path.join(testFolderPath))
-        .withPrompts({
-          name: "foo",
-          type: "typescript-express",
-          upgrade: false
-        });
-
-      expect(fs.existsSync(`${testFolderPath}/src/index.ts`)).toBe(true);
-      expect(
-        fs.existsSync(`${testFolderPath}/src/__tests__/stub.test.ts`)
-      ).toBe(true);
-      expect(fs.existsSync(`${testFolderPath}/package.json`)).toBe(true);
-      expect(fs.existsSync(`${testFolderPath}/tsconfig.json`)).toBe(true);
-      expect(fs.existsSync(`${testFolderPath}/.gitignore`)).toBe(true);
-      expect(fs.existsSync(`${testFolderPath}/.prettierignore`)).toBe(true);
-      done();
-    } catch (error) {
-      done();
-    }
+  test("should create all the files in tmp directory", () => {
+    expect(fs.existsSync(`${testFolderPath}/src/index.ts`)).toBe(true);
+    expect(fs.existsSync(`${testFolderPath}/src/__tests__/stub.test.ts`)).toBe(
+      true
+    );
+    expect(fs.existsSync(`${testFolderPath}/package.json`)).toBe(true);
+    expect(fs.existsSync(`${testFolderPath}/tsconfig.json`)).toBe(true);
+    expect(fs.existsSync(`${testFolderPath}/.gitignore`)).toBe(true);
+    expect(fs.existsSync(`${testFolderPath}/.prettierignore`)).toBe(true);
   });
 });
