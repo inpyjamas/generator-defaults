3cda8db4c9181ce0578f856daa5dce6dc3fac185
fabianmoronzirfas <fabianmoronzirfas@protonmail.ch>
2020-06-09 19:13:04 +0200

    remove build dir

--- generators/app/index.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-const in_pyjamas_generator_1 = require("./lib/in-pyjamas-generator");
-module.exports = in_pyjamas_generator_1.InPyjamasGenerator;
--- generators/app/lib/dependencies.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.dependencies = void 0;
-exports.dependencies = {
-    "typescript-express": ["cors", "dotenv", "express", "morgan"],
-    "typescript-jekyll-webpack": ["bulma"],
-    basiljs: ["basiljs/basil.js#develop"],
-};
